39090a6f88afb32a68975535e0efb697c9032faa
LarryCollinsAka <clatehlarry@gmail.com>
2025-11-15 14:44:48 +0100

    FIX: drizzle push

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'drizzle-kit';
 import 'dotenv/config';
 
 export default defineConfig({
-  schema: './lib/db.ts', // 
+  schema: './lib/db.ts', 
   out: './lib/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.POSTGRES_URL!,
+    url: process.env.POSTGRES_URL_NON_POOLING!,
   },
   verbose: true,
   strict: true,
